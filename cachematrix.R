c2e737fe1649e2903f8a22fde18a6cee5a5ab2d5
Filip Milojkovic <filip.milo@gmx.de>
2015-03-13 17:47:07 +0100

    Thus fur uncommented solution to rprog Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,41 @@
-## [Put comments here that describe what your functions do]
+## Dear reviewer, 
+# thank you for taking the time to look into my humble
+# contribution! 
 
-makeCacheMatrix <- function(x = matrix()) {
+####################################################
+## The following function yields a vector, which is actually a list, 
+## 
 
+makeCacheMatrix <- function(x = numeric()) {
+  sol <- NULL
+  set <- function(y) {
+    x <<- y
+    sol <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) sol <<- solve
+  getSolve <- function() sol
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
+
+
+
+#############################################
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  sol <- x$getSolve()
+  if(!is.null(sol)) {
+    message("getting cached data")
+    return(sol)
+  }
+  data <- x$get()
+  sol <- solve(data, ...)
+  x$setSolve(sol)
+  sol
 }
+
+# and for the fun of the game, a random cat content for some feel good factor
+# http://www.gifbin.com/986145
